b39eca264fc90b3bb8651d8992e355253b773e2d
geeseek <wangshi.tech@gmail.com>
2013-07-10 15:47:38 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var fs = require('rs');
+//var fs = require('rs');
 var app = express.createServer(express.logger());
-var contents = fs.readFileSync('./index.html');
-var buffer = new Buffer(50);
-buffer.write(contents);
+//var contents = fs.readFileSync('./index.html');
+//var buffer = new Buffer(50);
+//buffer.write(contents);
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send('hello world2!');
 });
 
 var port = process.env.PORT || 5000;
